c30a6e8a5994b8a88e328c7a7f027bd9e2a10943
griffi-gh <prasol258@gmail.com>
2021-12-22 20:59:40 +0200

    AAA

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-import sharp from 'sharp';
-import ffmpeg from 'ffmpeg';
 import * as fs from 'fs/promises';
 import * as path from 'path';
+import ffmpeg from 'ffmpeg';
+import sharp from 'sharp';
+import zlib from 'zlib';
+
+// Constants
+const targetWidth = 255;
+const targetHeight = 255;
+
+// Output
+const output = [];
 
 // Define functions
 const getNumeric = s => parseInt(s.replace(/\D/g,''));
@@ -26,17 +34,41 @@ await video.fnExtractFrameToJPG(framesDir, {
   file_name: '%i'
 });
 
-// List files
+// Process files
 console.log('Processing images...')
 const framesFiles = await fs.readdir(framesDir);
 framesFiles.sort((a, b) => getNumeric(a) - getNumeric(b));
 for(const frameFileName of framesFiles) {
   const framePath = path.resolve(path.join(framesDir, frameFileName));
-  const frameData = await sharp(framePath).toColourspace('b-w').raw().toBuffer();
+  const frameData = await sharp(framePath)
+    .threshold()
+    .resize({width: targetWidth, height: targetHeight})
+    .toColorspace('b-w')
+    .raw().toBuffer();
+  let currentColor = false;
+  let repeats = 0;
+  for(let i = 0; i < frameData.length; i++) {
+    const pixelColor = (frameData[i] > 0);
+    if(pixelColor == currentColor) {
+      repeats++;
+      if(repeats >= 0xFF) {
+        repeats -= 0xFF;
+        output.push(0xFF);
+      }
+    } else {
+      output.push(repeats);
+      currentColor = pixelColor;
+      repeats = 0;
+    }
+  }
+  if(repeats > 0) output.push(repeats);
+  break;
 }
 
 // Delete the frames
 console.log('Cleaning up...');
 try { await fs.rm(framesDir, {recursive: true, force: true}); } catch {}
 
+// Done, write the output file
+// TODO write out
 console.log('Done!');
