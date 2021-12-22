a83b791ec21e1471c645ec82f802ddf4a60b114c
griffi-gh <prasol258@gmail.com>
2021-12-22 18:07:11 +0200

    u

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-import jpeg from 'jpeg-js';
+import sharp from 'sharp';
 import ffmpeg from 'ffmpeg';
 import * as fs from 'fs/promises';
 import * as path from 'path';
+
+// Define functions
+const getNumeric = s => parseInt(s.replace(/\D/g,''));
 
 // Parse args
 const args = process.argv.slice(2);
@@ -10,4 +13,30 @@ const file = path.resolve(process.cwd(), args.shift());
 // Load file
 console.log(`Loading file "${file}"`);
 const video = await new ffmpeg(file)
-console.log(video.metadata);
+
+// Create directory
+const framesDir = path.resolve(process.cwd(), '.frames/');
+try { await fs.rm(framesDir, {recursive: true, force: true}); } catch {}
+await fs.mkdir(framesDir);
+
+// Split
+console.log(`Splitting to "${framesDir}"`);
+await video.fnExtractFrameToJPG(framesDir, {
+  every_n_frames: 2,
+  file_name: '%i'
+});
+
+// List files
+console.log('Processing images...')
+const framesFiles = await fs.readdir(framesDir);
+framesFiles.sort((a, b) => getNumeric(a) - getNumeric(b));
+for(const frameFileName of framesFiles) {
+  const framePath = path.resolve(path.join(framesDir, frameFileName));
+  const frameData = await sharp(framePath).toColourspace('b-w').raw().toBuffer();
+}
+
+// Delete the frames
+console.log('Cleaning up...');
+try { await fs.rm(framesDir, {recursive: true, force: true}); } catch {}
+
+console.log('Done!');
