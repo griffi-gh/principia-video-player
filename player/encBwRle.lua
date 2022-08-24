4c8126d0f7b5fdb77cedd813c640d7329a82a94f
prasol258@gmail.com <prasol258@gmail.com>
2022-08-24 22:15:27 +0200

    Run at framerate + fix upside down player

--- player/encBwRle.lua
+++ player/encBwRle.lua
@@ -12,7 +12,8 @@ function step()
   if skp <= 0 then
     skp = 4 
     -- PARSE A SINGLE FRAME ----
-    for i=1,25 do
+    local B
+    while not B do
       local d,T,W = D()
       cur = cur + 1
       c = not c
@@ -27,13 +28,14 @@ function step()
         end
         for i=1,d do
           T = c and 255 or 0
-          this:set_sprite_texel(x,y,T,T,T,255)
+          this:set_sprite_texel(x,h-y,T,T,T,255)
           x = x + 1
           if x >= w then
             x = 0
             y = y + 1
             if y >= h then
               y = 0
+              B = true
             end
           end
         end
