1242aa3c625e70c53b216877dcecab2d683ad31e
prasol258@gmail.com <prasol258@gmail.com>
2022-08-24 19:58:41 +0200

    player

--- lua_datastring.js
+++ lua_datastring.js
@@ -1,41 +1,38 @@
-export default function(buf) {
+const escapeMap = [];
+escapeMap[7] = 'a';
+escapeMap[8] = 'b';
+escapeMap[9] = 't';
+escapeMap[10] = 'n';
+escapeMap[11] = 'v';
+escapeMap[12] = 'f';
+escapeMap[13] = 'r';
+escapeMap[34] = '"';
+escapeMap[92] = '\\';
+
+export default function datastring(buf) {
   let str = '"';
-  let dangerEscape = false;
-  for (const byte of buf) {
-    const escapeMap = [];
-    {
-      escapeMap[7] = 'a';
-      escapeMap[8] = 'b';
-      escapeMap[9] = 't';
-      escapeMap[10] = 'n';
-      escapeMap[11] = 'v';
-      escapeMap[12] = 'f';
-      escapeMap[13] = 'r';
-      escapeMap[34] = '"';
-      escapeMap[92] = '\\';
-    }
+  for (let i = 0; i < buf.length; i++) {
+    const byte = buf[i];
     if (escapeMap[byte]) {
       str += '\\' + escapeMap[byte];
-      dangerEscape = false;
     } else if ((byte >= 32) && (byte <= 126)) {
-      if (dangerEscape && (byte >= 48) && (byte <= 57)) {
-        //TODO modify dangerous escape seq
-        //     like this: \9 => \009
-        //     instead of doing this
-        //     saves 1 character per number
-        //     if escape sequence is 2 chars
-        //     before: \69\48 <...escape unsafe>
-        //     after:  \0690  <...escape safe>  
-        str += '\\' + byte.toString();
-        dangerEscape = true;
-      } else {
-        str += String.fromCharCode(byte);
-        dangerEscape = false;
-      }
+      str += String.fromCharCode(byte);
     } else {
       const byteStr = byte.toString();
-      str += '\\' + byteStr;
-      dangerEscape = byteStr.length !== 3;
+      str += '\\';
+      // If the next character is number, pad with 0
+      // To prevent it from being trated as a part
+      // of the escape sequence
+      if (
+        (buf[i+1] != null) &&
+        (buf[i+1] >= 48) &&
+        (buf[i+1] <= 57) &&
+        (byteStr.length !== 3)
+      ) {
+        str += byteStr.padStart(3, '0');
+      } else {
+        str += byteStr;
+      }
     }
   }
   return str + '"';
---
+++ player/encBwRle.lua
@@ -0,0 +1,48 @@
+local cur = 0
+local function D(i)
+  return DATA:byte(i or cur, -1)
+end
+local 
+  w,h, --Width, Height
+  skp,cur,fcr, --Skip, Cursor, FramebufferCursor
+  fb,c --Framebuffer, Color
+= 
+  D(),D(1),
+  0,3,1,
+  {} --c = nil
+
+--Assumes 60 fps; 60/4 = 15 fps
+function step()
+  if skp == 0 then
+    skp = 4 
+    -- PARSE A SINGLE FRAME ----
+    local o = 1
+    while o do
+      local d,x,y,T = D(),0,0
+      if not d then return end
+      cur = cur + 1
+      c = not c
+      for i=1,d do
+        fb[fcr] = c
+        fcr = fcr + 1
+        if fcr > (w*h+1) then
+          -- RENDER FRAME --
+          for i=1,#fb do
+            T = fb[i] and 255 or 0
+            this:set_sprite_texel(x,y,T,T,T,255)
+            x = x + 1
+            if x >= w then
+              y = y + 1
+            end
+          end
+          ------------------
+          fb = {}
+          fcr = 1
+          o = N --<nil>
+        end
+      end
+    end
+    ---------------------------
+  end 
+  skp = skp - 1
+end
