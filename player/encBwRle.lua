c701f66a9d5caf7f507af06a3f6391e30dc1d9f1
prasol258@gmail.com <prasol258@gmail.com>
2022-08-24 21:10:45 +0200

    getting there :p

--- player/encBwRle.lua
+++ player/encBwRle.lua
@@ -1,52 +1,52 @@
-local cur = 3
+local cur = 4
 local function D(i)
-  return DATA:byte(i or cur, -1)
+  return DATA:byte(i or cur)
 end
 local 
   w,h, --Width, Height
-  skp,cur,fcr, --Skip, Cursor, FramebufferCursor
-  fb,c --Framebuffer, Color
+  skp,cur, --Skip, Cursor
+  x,y,c-- x , y, color (nil)
 = 
   D(),D(2),
-  0,3,1,
-  {} --c = nil
+  0,3,
+  0,0
 
 --Assumes 60 fps; 60/4 = 15 fps
 function step()
-  if skp == 0 then
+  if skp <= 0 then
     skp = 4 
     -- PARSE A SINGLE FRAME ----
-    local o = 1
-    while o do
-      local d,x,y,T = D(),0,0
-      if not d then return end
+    for i=1,25 do
+      --game:show_numfeed(cur)
+      local d,T = D(),c and 255 or 0
       cur = cur + 1
       c = not c
-      for i=1,d do
-        fb[fcr] = c
-        fcr = fcr + 1
-        if fcr > (w*h+1) then
-          -- RENDER FRAME --
-          for i=1,#fb do
-            T = fb[i] and 255 or 0
-            this:set_static_sprite_texel(x,y,T,T,T,255)
-            x = x + 1
-            if x >= w then
-              y = y + 1
-            end
+      if d > 255 then
+        while true do
+          d = d + D()
+          cur = cur + 1
+          if D(cur - 1) ~= 255 then
+            break
           end
-          ------------------
-          fb = {}
-          fcr = 1
-          o = nil
+        end
+      end
+      for i=1,d do
+        this:set_sprite_texel(x,y,T,T,T,255)
+        x = x + 1
+        if x > w then
+          x = 0
+          y = y + 1
+          if y > h then
+            y = 0
+          end
         end
       end
     end
     ---------------------------
   end 
   skp = skp - 1
+  this:draw_sprite(0, 0, 0, 2, 1, 0, 0, w, h)
 end
 function init()
   this:init_draw(256, 128)
-  this:add_static_sprite(0, 0, 0, 2, 1, 0, 0, w, h)
 end
