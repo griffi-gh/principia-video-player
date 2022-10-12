7d1f3a0f55d969f60ed912289c4fe684f25e44d3
prasol258@gmail.com <prasol258@gmail.com>
2022-10-12 15:09:20 +0200

    fvck

--- common/luastr.js
+++ common/luastr.js
@@ -1,23 +1,23 @@
-// ASII CODE, ESCAPE, LUA STR REPR
+// ASII CODE, ESCAPE, LUA STR REPR (optional)
 const ESCAPE = [
   //these three are confirmed to break stuff
-  [10, 'n', '\\n'], //must be first
-  [13, 'r', '\\r'],
-  [0,  '0', '\\0'],
+  [10, 'n'], //must be first
+  [13, 'r'],
+  [0,  '0'],
   //maybe some are actually not needed:
   //but some of these may break save files
-  [1,  '1', '\\1'],
-  [2,  '2', '\\2'],
-  [3,  '3', '\\3'],
-  [4,  '4', '\\4'],
-  [5,  '5', '\\5'],
-  [6,  '6', '\\6'],
-  [7,  '7', '\\7'],
-  [8,  '8', '\\8'],
+  [1,  '1'],
+  [2,  '2'],
+  [3,  '3'],
+  [4,  '4'],
+  [5,  '5'],
+  [6,  '6'],
+  [7,  '7'],
+  [8,  '8'],
 ];
 const LUA_PRE_STR = Buffer.from('([=[\n');
 const LUA_POST_STR = Buffer.from(']=])' + ESCAPE.map(esc => (
-  `:gsub('\\n${esc[1]}','${esc[2]}')`
+  `:gsub('\\n${esc[1]}','${esc[2] ?? ('\\' + esc[1])}')`
 )).join(''));
 
 export default function(buf) {
