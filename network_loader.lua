e5df1c48a02f6a9f799b96447167ce3a8295bf6c
griffi-gh <prasol258@gmail.com>
2022-10-14 02:22:58 +0200

    Update network_loader.lua

--- network_loader.lua
+++ network_loader.lua
@@ -2,14 +2,18 @@ local host = "digitalcitrus.atwebpages.com"
 local port = 80
 local path = "/index.html"
 --
-if require then
-  socket = require'socket'
-end
+if require then socket = require'socket' end
+if not print then print = function() end end
 assert(socket, "Not compatible with mobile devices, download HD version manually or use the low quality one")
 local socket = socket
-local tcp = socket.tcp()
-tcp:connect(host, port)
-tcp:send("GET "..path.." HTTP/1.1\r\nHost: "..host.."\r\n\r\n")
-local result = tcp:receive('*a')
-tcp:close()
-print(result:find("\r\n\r\n"))
+local result
+do
+  local tcp = socket.tcp()
+  tcp:connect(host, port)
+  tcp:send("GET "..path.." HTTP/1.1\r\nHost: "..host.."\r\n\r\n")
+  result = tcp:receive('*a')
+  tcp:close()
+end
+local _, split = result:find("\r\n\r\n")
+local text = result:sub(split, #result)
+assert(load(text))()
